b4e4d1e994a1a7cfaff430eed30037ec2fa09b98
Darya Razmanova <rdashak8@gmail.com>
2022-04-25 08:37:18 +0800

    Add files to domain (Author, Book, Comment, Genre)

---
+++ src/main/java/com/samsung/domain/Book.java
@@ -0,0 +1,32 @@
+package com.samsung.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(targetEntity = Author.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "book")
+    private List<Comment> commentList;
+}
